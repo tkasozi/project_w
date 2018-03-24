35bd7347942a200b36a99e082d0952028ee44e72
Talik Kasozi <talik.aziizi@gmail.com>
2018-03-24 01:39:00 -0400

    first commit

---
+++ src/components/welcome-login-signup.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+
+/**
+ * User login/ (sign up — option)
+ * This component is designed for the welcome page
+ * (on a user-based app) 
+ */
+
+let main_test = () =>{
+    console.log("main_test")
+}
+
+export class SignUp extends Component{
+    render(){
+        return(
+            <div>
+                <input placeholder="username" type="text"/><br/>
+                <input placeholder="password"type="password"/> <br/>   
+                <input type="submit" value="Sign Up"/>
+            </div>
+        );
+    }
+}
+
+class WelcomeLogin extends Component{
+    render(){
+        return(
+            <div>
+                <input placeholder="username" type="text"/><br/>
+                <input placeholder="password"type="password"/> <br/>   
+                <button type="submit" onClick={main_test} value="Login">Login</button> 
+                <a href="#">Forgot password?</a>
+            </div>
+        );
+    }
+}
+
+export default WelcomeLogin;
