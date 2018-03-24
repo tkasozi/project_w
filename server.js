62ab51468a00c77f838c648c4a709c36a27c23c3
Talik Kasozi <talik.aziizi@gmail.com>
2018-03-24 04:32:04 -0400

    db

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+/**
+Create Table IF NOT EXISTS user(
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+Username VARCHAR(24) NOT NULL,
+Password VARCHAR(24) NOT NULL
+);
+
+INSERT INTO user (Username, Password) VALUES("tkasozi","Veex");
+
+SELECT * FROM user LIMIT 1000;
+
+DROP Table IF EXISTS user;
+ */
